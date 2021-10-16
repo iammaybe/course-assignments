061a94bba986df6219ef099f7d26066c596b7964
maybe <iammaybe7@gmail.com>
2021-10-16 20:54:00 +0200

    add functionality of resetting the game

--- [project] Pig Game/script.js
+++ [project] Pig Game/script.js
@@ -14,14 +14,24 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -64,7 +74,7 @@ btnHold.addEventListener('click', function () {
 
     // 2. Check if player's score is >= 100
 
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -81,4 +91,5 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-// document.getElementById(`score--${activePlayer}`).textContent = 100;
+// Resetting the game functionality
+btnNew.addEventListener('click', init);
