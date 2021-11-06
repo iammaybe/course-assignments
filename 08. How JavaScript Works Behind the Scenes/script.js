98ab8071779a0bb6b6151601cdde8b1242c318e2
maybe <iammaybe7@gmail.com>
2021-11-06 23:59:10 +0100

    Add 'addDecl', 'addExpr' and 'addArrow' functions

--- 08. How JavaScript Works Behind the Scenes/script.js
+++ 08. How JavaScript Works Behind the Scenes/script.js
@@ -43,10 +43,26 @@ calcAge(1991);
 // Hoisting and TDZ in Practice
 
 // Variables
-console.log(me);
-console.log(job);
-console.log(year);
+// console.log(me);
+// console.log(job);
+// console.log(year);
 
-var me = 'Jonas';
-let job = 'teacher';
-const year = 1991;
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
