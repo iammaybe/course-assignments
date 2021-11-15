eb0cf9c1e9fe7265aa8e5ecca0ce9f66f28fbab6
maybe <iammaybe7@gmail.com>
2021-11-15 23:32:05 +0100

    Add example 5: 'jonas' object

--- 08. How JavaScript Works Behind the Scenes/script.js
+++ 08. How JavaScript Works Behind the Scenes/script.js
@@ -51,57 +51,67 @@
 // let job = 'teacher';
 // const year = 1991;
 
-// Functions
-console.log(addDecl(2, 3));
-// console.log(addExpr(2, 3));
-console.log(addArrow);
-// console.log(addArrow(2, 3));
+// // Functions
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow);
+// // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-  return a + b;
-}
+// function addDecl(a, b) {
+//   return a + b;
+// }
 
-const addExpr = function (a, b) {
-  return a + b;
-};
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
 
-var addArrow = (a, b) => a + b;
+// var addArrow = (a, b) => a + b;
 
 // Example 1
 
-console.log(numProducts);
-if (!numProducts) deleteShoppingCart();
+// console.log(numProducts);
+// if (!numProducts) deleteShoppingCart();
 
-var numProducts = 10;
+// var numProducts = 10;
 
-function deleteShoppingCart() {
-  console.log('All products deleted!');
-}
+// function deleteShoppingCart() {
+//   console.log('All products deleted!');
+// }
 
 // Example 2
 
-var x = 1;
-let y = 2;
-const z = 3;
+// var x = 1;
+// let y = 2;
+// const z = 3;
 
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
 
 // Example 3
 
-console.log(this); // window
+// console.log(this); // window
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this); // undefined
-};
-calcAge(1991);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this); // undefined
+// };
+// calcAge(1991);
 
 // Example 4
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  console.log(this); // window
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this); // window
+// };
+// calcAgeArrow(1980);
+
+// Example 5
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+  },
 };
-calcAgeArrow(1980);
+jonas.calcAge();
