917b25c316f856ff02f9723ba2e60c343947a49d
maybe <iammaybe7@gmail.com>
2021-11-18 22:06:00 +0100

    Add new feature to 'calcAge' method and add example 6: 'matilda' object

--- 08. How JavaScript Works Behind the Scenes/script.js
+++ 08. How JavaScript Works Behind the Scenes/script.js
@@ -112,6 +112,16 @@ const jonas = {
   year: 1991,
   calcAge: function () {
     console.log(this);
+    console.log(2037 - this.year);
   },
 };
 jonas.calcAge();
+
+// Example 6
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
