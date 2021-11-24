918753f06caa3840f5ee486ead421f90a246313d
maybe <iammaybe7@gmail.com>
2021-11-24 20:25:44 +0100

    Add array and use destructuring on it

--- 09. Data Structures, Modern Operators and Strings/script.js
+++ 09. Data Structures, Modern Operators and Strings/script.js
@@ -27,3 +27,12 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr; // destructuring
+console.log(x, y, z); // 2 3 4
+console.log(arr); // [2, 3, 4]
