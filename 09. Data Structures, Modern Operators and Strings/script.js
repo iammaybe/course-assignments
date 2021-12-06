1a1a659b3a4228b500e37e00902dc1824ce49c1b
maybe <iammaybe7@gmail.com>
2021-12-06 20:47:23 +0100

    Add some logic to last example

--- 09. Data Structures, Modern Operators and Strings/script.js
+++ 09. Data Structures, Modern Operators and Strings/script.js
@@ -68,7 +68,10 @@ console.log(weekdays); // {thu: {…}, fri: {…}}
 
 // 2) Functions
 const add = function (...numbers) {
-  console.log(numbers); // [2, 3] / [5, 3, 7, 2] / [8, 2, 5, 3, 2, 1, 4]
+  // console.log(numbers); // [2, 3] / [5, 3, 7, 2] / [8, 2, 5, 3, 2, 1, 4]
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
 };
 add(2, 3);
 add(5, 3, 7, 2);
