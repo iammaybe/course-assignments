5327535583965b780db150989f75e1e76418ecbc
maybe <iammaybe7@gmail.com>
2022-01-12 00:16:10 +0100

    Create basic structure

---
+++ Course 2/Section 11/Project 7 - ToDo List/script.js
@@ -0,0 +1,17 @@
+let todoInput, errorInfo, addBtn, ulList;
+
+const main = () => {
+  prepareDOMElements();
+  prepareDOMEvents();
+};
+
+const prepareDOMElements = () => {
+  todoInput = document.querySelector('.todo-input');
+  errorInfo = document.querySelector('.error-info');
+  addBtn = document.querySelector('.btn-add');
+  ulList = document.querySelector('.todolist ul');
+};
+
+const prepareDOMEvents = () => {};
+
+document.addEventListener('DOMContentLoaded', main);
