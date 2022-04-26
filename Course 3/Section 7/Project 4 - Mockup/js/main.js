a3e78b10cdd6efc65c2db0ec01a264e263245147
maybe <iammaybe7@gmail.com>
2022-04-26 19:00:46 +0200

    Fix scrollspy on desktop and optimize main.js

--- Course 3/Section 7/Project 4 - Mockup/js/main.js
+++ Course 3/Section 7/Project 4 - Mockup/js/main.js
@@ -1,7 +1,32 @@
-const nav = document.querySelector('.navbar-collapse');
+let nav, navLinksWrapper, navLinks;
 
-document.addEventListener('click', () => {
+const main = () => {
+  prepareDOMElements();
+  prepareDOMEvents();
+};
+
+const prepareDOMElements = () => {
+  nav = document.querySelector('.navbar-collapse');
+  navLinksWrapper = document.querySelector('.nav-desktop-links');
+  navLinks = document.querySelectorAll('.nav-desktop-link');
+};
+
+const prepareDOMEvents = () => {
+  document.addEventListener('click', closeNav);
+  navLinksWrapper.addEventListener('click', checkActive);
+};
+
+const closeNav = () => {
   if (nav.classList.contains('show')) {
     nav.classList.remove('show');
   }
-});
+};
+
+const checkActive = e => {
+  if (!e.target.closest('.nav-desktop-link').classList.contains('active')) {
+    navLinks.forEach(navLink => navLink.classList.remove('active'));
+    e.target.classList.add('active');
+  }
+};
+
+document.addEventListener('DOMContentLoaded', main);
