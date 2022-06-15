1ae0a936938a9a170c44f58f517b43fcbeba37be
maybe <iammaybe7@gmail.com>
2022-06-15 16:52:33 +0200

    Add 'checkInput' function

--- Course 4/Section 5/Project 4 - Magic 8 Ball/js/script.js
+++ Course 4/Section 5/Project 4 - Magic 8 Ball/js/script.js
@@ -9,9 +9,22 @@ const answersArr = [
   'Nie chcesz znać odpowiedzi na to pytanie... :/',
 ];
 
+const checkInput = () => {
+  if (input.value !== '' && input.value.slice(-1) === '?') {
+    generateAnswer();
+    error.textContent = '';
+  } else if (input.value !== '' && input.value.slice(-1) !== '?') {
+    error.textContent = 'Pytanie musi być zakończone znakiem "?".';
+    answer.textContent = '';
+  } else {
+    error.textContent = 'Musisz zadać jakieś pytanie!';
+    answer.textContent = '';
+  }
+};
+
 const generateAnswer = () => {
   const number = Math.floor(Math.random() * answersArr.length);
   answer.innerHTML = `<span>Odpowiedź</span>: ${answersArr[number]}`;
 };
 
-ball.addEventListener('click', generateAnswer);
+ball.addEventListener('click', checkInput);
