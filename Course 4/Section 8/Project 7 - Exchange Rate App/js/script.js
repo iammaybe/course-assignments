cfe8eb52e762cc6c50c9c0e3ad35a3dbdeb0819e
maybe <iammaybe7@gmail.com>
2022-07-14 21:11:49 +0200

    Add 'calculate' function

---
+++ Course 4/Section 8/Project 7 - Exchange Rate App/js/script.js
@@ -0,0 +1,25 @@
+const amountOne = document.querySelector('.amount-one');
+const currencyOne = document.querySelector('#currency-one');
+const amountTwo = document.querySelector('.amount-two');
+const currencyTwo = document.querySelector('#currency-two');
+const swapBtn = document.querySelector('.swap');
+const rateInfo = document.querySelector('.rate-info');
+
+const calculate = () => {
+  const URL = `https://api.exchangerate.host/latest?base=${currencyOne.value}&symbols=${currencyTwo.value})`;
+
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+      const rate = data.rates[currencyTwo.value];
+      amountTwo.value = (amountOne.value * rate).toFixed(2);
+      rateInfo.textContent = `1 ${currencyOne.value} = ${rate.toFixed(4)} ${
+        currencyTwo.value
+      }`;
+    });
+};
+
+currencyOne.addEventListener('change', calculate);
+currencyTwo.addEventListener('change', calculate);
+amountOne.addEventListener('input', calculate);
+calculate();
